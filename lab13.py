130f3da6a6e1bbba1824fa88a9a2a0e268efa965
ashwon13 <57849857+ashwon13@users.noreply.github.com>
2020-10-08 22:16:42 -0400

    Update lab13.py

--- lab13.py
+++ lab13.py
@@ -2,9 +2,10 @@
 #Date: 10/6/2020
 from Workers import *
 from random import randint 
+from pyKarel import *
 
 def work_one_row(arg, m):
-    for k in range(m):
+    for k in range(m-1):
         arg.doYourThing()
         arg.move()
       
@@ -21,7 +22,7 @@ def work_square(arg, n):
 def main():
     size = raw_input("Which 'field' (3, 5, 8)?")
     wld=World("field"+size, delay=0.1)
-    if randint(0,1) == 0:
+    if randint(0,1) == 2:
         work_square(Harvester(wld), int(size)) 
     else: 
         work_square(Planter(wld), int(size) )
