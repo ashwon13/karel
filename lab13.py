103f58a529f1377fe9c9af8452005130ad7caf05
ashwon13 <57849857+ashwon13@users.noreply.github.com>
2020-10-08 20:02:43 -0400

    Create lab13.py

---
+++ lab13.py
@@ -0,0 +1,31 @@
+#Name:Ashwin Pulla
+#Date: 10/6/2020
+from Workers import *
+from random import randint 
+
+def work_one_row(arg, m):
+    for k in range(m):
+        arg.doYourThing()
+        arg.move()
+      
+def work_square(arg, n):
+    arg.turnToTheNorth() 
+    work_one_row(arg, n)
+    for k in range(n, 1, -1):
+        arg.turnRight() 
+        work_one_row(arg, k) 
+        arg.turnRight() 
+        work_one_row(arg, k) 
+    arg.doYourThing() 
+    
+def main():
+    size = raw_input("Which 'field' (3, 5, 8)?")
+    wld=World("field"+size, delay=0.1)
+    if randint(0,1) == 0:
+        work_square(Harvester(wld), int(size)) 
+    else: 
+        work_square(Planter(wld), int(size) )
+    wld.mainloop()
+
+if __name__=="__main__":
+   main()
